ff94433e79aff8adde45932e9989b300c5ce58f0
Nicola Papale <nico.ppl@protonmail.ch>
2017-09-21 16:50:43 +0200

    Introduce functional backend API

--- server/Main.hs
+++ server/Main.hs
@@ -1,45 +1,131 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes, TypeFamilies,
+    ViewPatterns, MultiParamTypeClasses, FlexibleInstances, DeriveGeneric,
+    DeriveDataTypeable, RankNTypes #-}
 module Main where
 
-import Control.Concurrent (MVar, modifyMVar_, newMVar, readMVar)
-import Control.Monad (forever, mapM_)
-import Data.Aeson
-       (FromJSON(parseJSON), ToJSON, (.:), decode, defaultOptions, encode,
-        genericToEncoding, toEncoding, withObject)
-import qualified Data.ByteString.Lazy as B
-import Data.ByteString.Lazy (ByteString)
-import qualified Data.ByteString.Lazy.Char8 as B8
-import Data.Text (Text)
+import qualified RoomConnection as RC
+
+import Control.Concurrent.MVar (MVar, modifyMVar, newMVar, readMVar)
+import Control.Monad.IO.Class (liftIO)
+import Data.List (uncons)
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
+import Data.Maybe (maybe)
 import qualified Data.Text as T
-import GHC.Generics
-import qualified Network.WebSockets as WS
+import Data.Text (Text)
 
-type Client = WS.Connection
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Wai.Routes as Routes
+import Wai.Routes (Handler, RouteM, created201, notFound404)
+import qualified Network.Wai.Application.Static as SWai
+import qualified Network.Wai as Wai
+import qualified Text.StringRandom as Rand
 
-type ServerState = [Client]
+type RoomID = Text
+type RoomMap = Map RoomID RC.Room
+type GlobalState = MVar RoomMap
+type MutState a = GlobalState -> IO a
+data Netpinary = Netpinary GlobalState
 
-broadcast :: ByteString -> ServerState -> IO ()
-broadcast msg clients = do
-    B8.putStrLn msg
-    mapM_ (flip WS.sendTextData msg) clients
+{-| In the future, we add redirections to the proper WebSockets urls -}
+Routes.mkRoute "Netpinary" [Routes.parseRoutes|
+/ws/games/pintclone/+[Text] WebSocketAPI GET
+/rooms/create CreateRoom POST
+/rooms/showAll ShowRooms GET
+/ StaticContent GET
+|]
 
-serverApp :: MVar ServerState -> WS.ServerApp
-serverApp state pending = do
-    conn <- WS.acceptRequest pending
-    WS.forkPingThread conn 30
-    clients <- readMVar state
-    newclient <- return $ conn
-    modifyMVar_ state $ return . (:) newclient
-    talk conn state
+tshow :: Show a => a -> Text
+tshow = T.pack . show
 
-talk :: WS.Connection -> MVar ServerState -> IO ()
-talk conn state =
-    forever $ do
-        msg <- WS.receiveData conn
-        readMVar state >>= broadcast msg
+unpackGS :: Netpinary -> GlobalState
+unpackGS (Netpinary gs) = gs
+
+(<|-|) :: Show a => (Text -> b) -> a -> b
+(<|-|) f s = f $ tshow s
+
+-- *** -- *** -- SERVER STATE TYPE MANIPULATION -- *** -- *** --
+
+
+createRoom :: MutState RoomID
+createRoom gs = do
+    roomName <- Rand.stringRandomIO "[a-z0-9]{10}"
+    modifyMVar gs $ addRandRoom roomName
+    where -- Room name generated outside the lock to not clog it.
+        addRandRoom :: RoomID -> RoomMap -> IO (RoomMap, RoomID)
+        addRandRoom roomName rm = do
+            newRoom <- RC.newRoom
+            return ( Map.insert roomName newRoom rm, roomName )
+
+
+-- *** -- *** -- WEB REQUEST HANDLERS -- *** -- *** --
+
+
+getShowRooms :: Handler Netpinary
+getShowRooms = Routes.runHandlerM $ do
+    Netpinary globstate <- Routes.sub
+    rooms <- liftIO $ readMVar globstate
+    Routes.plain <|-| Map.keys rooms
+
+
+{-| Create a new room, returns redirection to game start lobby with created
+    room name when success. Returns an error when failure.
+    TODO: add failure condition? -}
+postCreateRoom :: Handler Netpinary
+postCreateRoom = Routes.runHandlerM $ do
+    Netpinary gs <- Routes.sub
+    roomid <- liftIO $ createRoom gs
+    Routes.plain roomid
+    Routes.status created201
+
+
+{-| Returns the URI of the ws/info of the given room if it exists.
+    otherwise, returns a 404.
+    If the room exists, but the username is already occupied, returns
+    a 403.
+    Possible inconsistent states: the post returns the resource for
+    the WebSocket, but when client requests the WebSocket, it doesn't
+    exist anymore.
+    TODO: use imported API module to handle the request body.-}
+-- postJoinRoom :: RoomID -> Handler Netpinary
+-- postJoinRoom roomid = Routes.runHandlerM $ do
+--     Netpinary globstate <- Routes.sub
+--     Routes.plain roomid
+
+
+getStaticContent :: Handler Netpinary
+getStaticContent =
+    Routes.mountedAppHandler $
+        SWai.staticApp $
+            SWai.defaultFileServerSettings "build"
+
+
+getWebSocketAPI :: [Text] -> Handler Netpinary
+getWebSocketAPI urlparts env req continue = do
+    roomMap <- readMVar $ unpackGS $ Routes.envSub env
+    maybe
+        (continue $ Wai.responseLBS notFound404 [] "Room doesn't exist")
+        (\(room, urltail) -> RC.app room urltail (Routes.waiReq req) continue)
+        $ do
+            (h,t) <- uncons urlparts
+            room <- Map.lookup h roomMap
+            return (room,t)
+
+
+-- *** -- *** -- APPLICATION HANDLERS -- *** -- *** --
+
+
+application :: Netpinary -> RouteM ()
+application gs = do
+    Routes.middleware Routes.logStdoutDev
+    Routes.route gs
+
+
+-- *** -- *** -- MAIN -- *** -- *** --
+
 
 main :: IO ()
 main = do
-    state <- newMVar []
-    WS.runServer "127.0.0.1" 9260 $ serverApp state
+    putStrLn "Starting server on port 8080"
+    globstate <- newMVar Map.empty
+    Warp.run 8080 $ Routes.waiApp $ application (Netpinary globstate)
---
+++ server/RoomConnection.hs
@@ -0,0 +1,177 @@
+{-# LANGUAGE OverloadedStrings, DuplicateRecordFields, NamedFieldPuns,
+    MonadComprehensions #-}
+module RoomConnection (Room, newRoom, app) where
+
+import qualified Network.WebSockets as WS
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.WebSockets as WaiWS
+import Control.Concurrent (MVar, modifyMVar, modifyMVar_, newMVar, withMVar)
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
+import Control.Monad (forever, forM_)
+import Control.Exception (finally)
+import Data.Text (Text, append)
+import Data.Maybe (isJust, maybe)
+import Network.HTTP.Types.Status (notFound404, badRequest400)
+
+data Channel
+    = Canvas
+    | Info
+
+instance Show Channel where
+    show Canvas = "canvas"
+    show Info = "info"
+
+
+type ClientName = Text
+data ClientSockets = ClientSockets
+    { canvas :: Maybe WS.Connection
+    , info :: WS.Connection
+    }
+
+type RoomState = Map ClientName ClientSockets
+type Room = MVar RoomState
+type MutRoom a = Room -> IO a
+
+
+newRoom :: IO Room
+newRoom = newMVar Map.empty
+
+
+{-| Given an acceptation condition `predicate`
+    and a transformation on a RoomState `trans` taking a connection,
+    will return a function that takes a connection, accepts and transforms
+    it only if `predicate` is true. -}
+tryDo
+    :: (RoomState -> Bool)
+    -> (WS.Connection -> RoomState -> RoomState)
+    -> WS.PendingConnection -> MutRoom (Maybe WS.Connection)
+tryDo predicate trans conn =
+    flip modifyMVar atomicTransform
+    where
+        atomicTransform roomState =
+            if predicate roomState then do
+                conn' <- WS.acceptRequest conn
+                WS.forkPingThread conn' 30
+                return ( trans conn' roomState, Just conn' )
+            else do
+                WS.rejectRequest conn "Unacceptable connection :/"
+                return ( roomState, Nothing )
+
+
+{-| Given `clientName` will accept a new connection if the clientName doesn't
+    already exists, otherwise refuses the connection. -}
+tryAddClient
+    :: ClientName
+    -> WS.PendingConnection -> MutRoom (Maybe WS.Connection)
+tryAddClient clientName =
+    tryDo predicate trans
+    where
+        predicate = Map.notMember clientName
+        trans conn = Map.insert clientName (ClientSockets Nothing conn)
+
+
+{-| Given `clientName` will accept a new canvas connection if the clientName
+    doesn't already have a Canvas connection and already exists.
+    otherwise refuses the connection-}
+tryAddCanvas
+    :: ClientName
+    -> WS.PendingConnection -> MutRoom (Maybe WS.Connection)
+tryAddCanvas clientName =
+    tryDo predicate trans
+    where
+        predicate = isJust . (\x -> canvas =<< Map.lookup clientName x)
+        trans conn = Map.adjust (\x -> x {canvas = Just conn}) clientName
+
+
+{-| Broadcast to all clients where filter(client) holds True msg,
+    on info channel.  -}
+broadcastInfo
+    :: WS.WebSocketsData msg
+    => (ClientName -> Bool)
+    -> msg -> MutRoom ()
+broadcastInfo filter' msg room =
+    let
+        sendifok :: Text -> ClientSockets -> IO ()
+        sendifok clientName sockets = do
+            if filter' clientName then
+                WS.sendTextData (info sockets) msg
+            else
+                return ()
+    in
+        withMVar room $ Map.foldMapWithKey sendifok
+
+
+{-| Atomically broadcast of the client leave notification and removing it from
+the room -}
+disconnect :: ClientName -> MutRoom ()
+disconnect clientName =
+    flip modifyMVar_ remAndBroadcast
+    where
+        remAndBroadcast :: RoomState -> IO RoomState
+        remAndBroadcast roomState = do
+            let disconnected = Map.delete clientName roomState
+            forM_-- TODO: use an external API json serialization module isntead of sending the raw name
+                (Map.elems disconnected)
+                (flip WS.sendTextData ("< " `append` clientName) . info)
+            return disconnected
+
+
+maintainConnection
+    :: WS.WebSocketsData msg
+    => WS.Connection
+    -> MutRoom ()
+    -> (msg -> MutRoom ())
+    -> MutRoom ()
+maintainConnection conn onDisconnect onRecieve room =
+    finally normalFlow $ onDisconnect room
+    where
+        normalFlow = forever $ do
+            msg <- WS.receiveData conn
+            onRecieve msg room
+
+
+-- TODO: properly handle invalid connections
+app' :: Channel -> ClientName -> Room -> WS.ServerApp
+app' channel clientName room pending = do
+    case channel of
+        Canvas -> do
+            Just conn <- tryAddCanvas clientName pending room
+            maintainConnection conn (disconnect clientName) ignoreData room
+            -- TODO: only accept and resend data from the current artist
+            -- connected.
+        Info -> do
+            Just conn <- tryAddClient clientName pending room
+            broadcastInfo (const True) ("> " `append` clientName) room
+            -- TODO: give new client info about who is already connected. Note,
+            -- this should be atomic to room variable, to avoid /tromperie/
+            maintainConnection conn (disconnect clientName) ignoreData room
+    where
+        ignoreData :: Text -> x -> IO ()
+        ignoreData = const . const $ return () -- ignore 2 arguments, IO ()
+
+
+app :: Room -> [Text] -> Wai.Application
+app room route request respond =
+    let
+        maybeWaiRespond =
+            case route of
+                ["canvas", clientName] ->
+                    WaiWS.websocketsApp
+                        (WS.defaultConnectionOptions)
+                        (app' Canvas clientName room)
+                        (request)
+
+                ["info", clientName] ->
+                    WaiWS.websocketsApp
+                        (WS.defaultConnectionOptions)
+                        (app' Info clientName room)
+                        (request)
+
+                _ ->
+                    Just $ Wai.responseLBS notFound404 [] "The websocket doesn't exist"
+
+        badRequestAnswer = -- TODO: understant when this is called
+            respond $ Wai.responseLBS badRequest400 [] "Not a WebSocket request"
+    in
+        maybe badRequestAnswer respond maybeWaiRespond
