2c6464b9060b59ab8149a74d555a78ced991cd16
Nicola Papale <nico.ppl@protonmail.ch>
2019-02-08 16:28:59 +0100

    Improve error messages on invalid field inputs
    * Now that the backend handles properly validation, the frontend can
      benefit from it. Now, the proper error message is displayed on error
      responses.

--- assets/script-create.js
+++ assets/script-create.js
@@ -1,6 +1,6 @@
 function sendCreateRequest() {
 	var xmlHttp = new XMLHttpRequest();
-	var inputUsername = document.getElementById("username").value;
+	var inputUsername = document.getElementById("name-input").value;
 	var params = JSON.stringify({
 		game: "classic",
 		username: inputUsername
@@ -12,11 +12,16 @@ function sendCreateRequest() {
 			switch (xmlHttp.status) {
 			case 201 :
 				var roomToJoin = xmlHttp.response;
-                sendJoinRequest(roomToJoin);
+				sendJoinRequest(roomToJoin);
 				break;
 			case 400 :
-				document.getElementById("username").classList.add("invalid");
-				document.getElementById("request").classList.add("invalid");
+				var re = /Request body deserialize error: (.*) at line 1 column.*/;
+				var cause = re.exec(xmlHttp.response)[1];
+				var message = cause?cause:"Invalid display name";
+				var field = document.getElementById("name-field");
+				field.dataset.errmsg = message;
+				field.classList.add("invalid");
+				document.getElementById("name-input").classList.add("invalid");
 				break;
 			default:
 				document.getElementById("request").style['display'] = "none";
--- assets/script-join.js
+++ assets/script-join.js
@@ -1,6 +1,6 @@
 function sendJoinRequest(roomToJoin) {
 	var xmlHttp = new XMLHttpRequest();
-	var inputUsername = document.getElementById("username").value;
+	var inputUsername = document.getElementById("name-input").value;
 	var params = JSON.stringify({
 		roomid: roomToJoin,
 		username: inputUsername
@@ -20,12 +20,19 @@ function sendJoinRequest(roomToJoin) {
 				], "/friendk/games/classic/index.html"]);
 				break;
 			case 409 :
-				document.getElementById("username").classList.add("taken");
-				document.getElementById("request").classList.add("taken");
+				var field = document.getElementById("name-field");
+				field.dataset.errmsg = "name already taken";
+				field.classList.add("invalid");
+				document.getElementById("name-input").classList.add("invalid");
 				break;
 			case 400 :
-				document.getElementById("username").classList.add("invalid");
-				document.getElementById("request").classList.add("invalid");
+				var re = /Request body deserialize error: (.*) at line 1 column.*/;
+				var cause = re.exec(xmlHttp.response)[1];
+				var message = cause?cause:"Invalid display name";
+				var field = document.getElementById("name-field");
+				field.dataset.errmsg = message;
+				field.classList.add("invalid");
+				document.getElementById("name-input").classList.add("invalid");
 				break;
 			default:
 				document.getElementById("request").style['display'] = "none";
