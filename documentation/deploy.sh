1489347304189977a36e95ebfd689197ad6133b0
Nicola Papale <nico.ppl@protonmail.ch>
2019-02-16 10:21:48 +0100

    Unify deployment setup
    Instead of relying of a script on the server, I integrated the string
    substitution in the deployment script. Now that the script is
    checked-in in the git tree, I can rely on it and know exactly where to
    look for if something goes wrong during the deployment process.

--- documentation/deploy.sh
+++ documentation/deploy.sh
@@ -2,10 +2,15 @@
 touch front/Pintclone.elm
 touch lobby/Main.elm
 make release-frontend
+for matchedfile in $(find build -name '*.js') ; do
+	sed -i "s|://localhost:8080|s://friendsketch.com|g" $matchedfile
+done
 ssh nicola@pi 'rm -r /home/nicola/server-setup/friendsketch/*'
 scp -rC build/* nicola@pi:/home/nicola/server-setup/friendsketch/
+/usr/bin/rm -r build/*
 cargo build --release --target armv7-unknown-linux-gnueabihf
 /usr/bin/arm-linux-gnueabihf-strip -s target/armv7-unknown-linux-gnueabihf/release/friendsketch
-ssh nicola@pi 'pkill friendsketch'
+ssh nicola@pi 'rm /home/nicola/server-setup/friendsketch-bin'
 scp -C target/armv7-unknown-linux-gnueabihf/release/friendsketch nicola@pi:/home/nicola/server-setup/friendsketch-bin
-ssh nicola@pi 'source /home/nicola/server-setup/deploy-friendsketch.sh'
+ssh nicola@pi 'systemctl --user restart friendsketch.service'
+
